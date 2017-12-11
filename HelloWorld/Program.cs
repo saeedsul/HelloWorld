6bf96ea2e786ba93cf9f87d118c76673f4efd89c
saeedsul <saeed.suleiman@hotmail.co.uk>
2017-12-11 23:01:05 +0000

    commit 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // test
+            // test2 
             Console.WriteLine("Hello World!");
         }
     }
